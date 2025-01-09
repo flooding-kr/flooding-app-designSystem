e190f052d2efc82d9154318cc9b09f1dc9d67844
iloveuhyeon <pyh1111802@naver.com>
2025-01-09 15:54:40 +0900

    Fix :: update file name and update pubspec.yaml

--- test/design_system_test.dart
+++ test/design_system_test.dart
@@ -1,12 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:design_system/design_system.dart';
+import 'package:flooding_app_design_system/flooding_app_design_system.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  test('adds one to input values', () {});
 }

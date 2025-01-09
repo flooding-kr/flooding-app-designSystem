89c0385aec2964b309edd3d8634db6f06399f9b5
youloveli <mymhj122@gmail.com>
2025-01-09 13:54:37 +0900

    글꼴 추가

--- lib/design_system.dart
+++ lib/design_system.dart
@@ -1,7 +1,126 @@
-library design_system;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class ExpoTypography {
+  static const _fontFamilyPretendard = 'Pretendard';
+
+  // Title 1
+  static const TextStyle title1Medium = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w500, // Medium
+    fontSize: 40,
+    height: 1.2, // 120%
+  );
+
+  // Title 2
+  static const TextStyle title2Medium = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w500, // Medium
+    fontSize: 36,
+    height: 1.2, // 120%
+  );
+
+  // Title 3
+  static const TextStyle title3SemiBold = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w600, // SemiBold
+    fontSize: 32,
+    height: 1.2, // 120%
+  );
+
+  static const TextStyle title3Regular = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w400, // Regular
+    fontSize: 32,
+    height: 1.2, // 120%
+  );
+
+  // Body 1
+  static const TextStyle body1SemiBold = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w600, // SemiBold
+    fontSize: 24,
+    height: 1.2, // 120%
+  );
+
+  static const TextStyle body1Regular = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w400, // Regular
+    fontSize: 24,
+    height: 1.2, // 120%
+  );
+
+  // Body 2
+  static const TextStyle body2SemiBold = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w600, // SemiBold
+    fontSize: 20,
+    height: 1.2, // 120%
+  );
+
+  static const TextStyle body2Regular = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w400, // Regular
+    fontSize: 20,
+    height: 1.2, // 120%
+  );
+
+  // Body 3
+  static const TextStyle body3SemiBold = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w600, // SemiBold
+    fontSize: 16,
+    height: 1.4, // 140%
+  );
+
+  static const TextStyle body3Regular = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w400, // Regular
+    fontSize: 16,
+    height: 1.6, // 160%
+  );
+
+  // Caption 1
+  static const TextStyle caption1SemiBold = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w600, // SemiBold
+    fontSize: 14,
+    height: 1.4, // 140%
+  );
+
+  static const TextStyle caption1Regular = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w400, // Regular
+    fontSize: 14,
+    height: 1.4, // 140%
+  );
+
+  // Caption 2
+  static const TextStyle caption2SemiBold = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w600, // SemiBold
+    fontSize: 12,
+    height: 1.5, // 150%
+  );
+
+  static const TextStyle caption2Medium = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w500, // Medium
+    fontSize: 12,
+    height: 1.5, // 150%
+  );
+
+  // Caption 3
+  static const TextStyle caption3SemiBold = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w600, // SemiBold
+    fontSize: 10,
+    height: 1.8, // 180%
+  );
+
+  static const TextStyle caption3Medium = TextStyle(
+    fontFamily: _fontFamilyPretendard,
+    fontWeight: FontWeight.w500, // Medium
+    fontSize: 10,
+    height: 1.8, // 180%
+  );
 }

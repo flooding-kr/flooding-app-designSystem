4f265fe64a3db2295c48632b2ad34fbc02b23ce4
parkyuhyeon <pyh1111802@naver.com>
2025-02-07 00:24:28 +0900

    linting design :: textfield 클래스 이름 변경, enum 적용

--- lib/src/utils/component/text_field.dart
+++ lib/src/utils/component/text_field.dart
@@ -8,7 +8,7 @@ enum FloodingTextFieldState {
   search,
 }
 
-class FloodingEmailTextField extends StatelessWidget {
+class FloodingTextField extends StatefulWidget {
   final FloodingTextFieldState textFieldState;
   final TextEditingController controller;
   final String? validator;
@@ -16,23 +16,35 @@ class FloodingEmailTextField extends StatelessWidget {
   final BorderRadius borderRadius;
   final TextStyle textStyle;
   final Color cursorColor;
+  final Color cursorErrorColor;
   final EdgeInsetsGeometry contentPadding;
   final String hintText;
   final TextStyle hintStyle;
   final Color hintColor;
   final TextStyle errorStyle;
   final Color errorColor;
+  final BorderSide errorBorderSide;
+  final VoidCallback? onEditingComplete;
+  final EdgeInsetsGeometry searchIconPadding;
+  final double searchIconSize;
+  final Color searchIconColor;
+  final EdgeInsetsGeometry passwordIconPadding;
+  final double passwordIconSize;
+  final Color passwordOnIconColor;
+  final Color passwordOffIconColor;
 
-  const FloodingEmailTextField({
+  const FloodingTextField({
     super.key,
     required this.textFieldState,
     required this.controller,
     required this.hintText,
     this.validator,
+    this.onEditingComplete,
     this.backgroundColor = FloodingColor.white,
     this.borderRadius = const BorderRadius.all(Radius.circular(6)),
     this.textStyle = FloodingTypography.body3Regular,
     this.cursorColor = FloodingColor.main600,
+    this.cursorErrorColor = FloodingColor.error,
     this.contentPadding = const EdgeInsets.symmetric(
       vertical: 12,
       horizontal: 16,
@@ -41,7 +53,27 @@ class FloodingEmailTextField extends StatelessWidget {
     this.hintColor = FloodingColor.gray500,
     this.errorStyle = FloodingTypography.caption2Medium,
     this.errorColor = FloodingColor.error,
+    this.errorBorderSide =
+        const BorderSide(color: FloodingColor.error, width: 1),
+    this.searchIconPadding = const EdgeInsets.symmetric(
+      horizontal: 28,
+      vertical: 12,
+    ),
+    this.searchIconSize = 36,
+    this.searchIconColor = FloodingColor.gray700,
+    this.passwordIconPadding =
+        const EdgeInsets.symmetric(horizontal: 16, vertical: 13),
+    this.passwordIconSize = 24,
+    this.passwordOffIconColor = FloodingColor.gray500,
+    this.passwordOnIconColor = FloodingColor.black,
   });
+
+  @override
+  State<FloodingTextField> createState() => _FloodingTextFieldState();
+}
+
+class _FloodingTextFieldState extends State<FloodingTextField> {
+  bool _isObscureText = true;
 
   String? _validateEmail(String? value) {
     if (value == null || value.isEmpty) {
@@ -72,49 +104,81 @@ class FloodingEmailTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      spacing: 4,
-      children: [
-        TextFormField(
-          controller: controller,
-          autovalidateMode: AutovalidateMode.onUnfocus,
-          validator: (value) {
-            if (textFieldState == FloodingTextFieldState.email) {
-              _validateEmail(value);
-            } else if (textFieldState == FloodingTextFieldState.password) {
-              _validatePassword(value);
-            } else if (textFieldState == FloodingTextFieldState.basic &&
-                validator != null) {
-              validator;
-            } else {
-              return null;
-            }
-            return null;
-          },
-          style: textStyle,
-          cursorColor: cursorColor,
-          obscureText:
-              textFieldState == FloodingTextFieldState.password ? true : false,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: backgroundColor,
-            contentPadding: contentPadding,
-            hintText: hintText,
-            hintStyle: hintStyle.copyWith(
-              color: hintColor,
-            ),
-            errorStyle: errorStyle.copyWith(
-              color: errorColor,
-            ),
-            label: ,
-            border: OutlineInputBorder(
-              borderRadius: borderRadius,
-              borderSide: BorderSide.none,
-            ),
-          ),
+    return TextFormField(
+      controller: widget.controller,
+      autovalidateMode: AutovalidateMode.onUnfocus,
+      validator: (value) {
+        if (widget.textFieldState == FloodingTextFieldState.email) {
+          return _validateEmail(value);
+        } else if (widget.textFieldState == FloodingTextFieldState.password) {
+          return _validatePassword(value);
+        } else if (widget.textFieldState == FloodingTextFieldState.basic &&
+            widget.validator != null) {
+          return widget.validator;
+        } else {
+          return null;
+        }
+      },
+      style: widget.textStyle,
+      cursorColor: widget.cursorColor,
+      cursorErrorColor: widget.cursorErrorColor,
+      onEditingComplete: widget.onEditingComplete,
+      obscureText: widget.textFieldState == FloodingTextFieldState.password
+          ? _isObscureText
+          : false,
+      decoration: InputDecoration(
+        suffixIcon: widget.textFieldState == FloodingTextFieldState.search
+            ? GestureDetector(
+                onTap: widget.onEditingComplete,
+                child: Padding(
+                  padding: widget.searchIconPadding,
+                  child: FloodingIcon.search(
+                    color: widget.searchIconColor,
+                    height: widget.searchIconSize,
+                    width: widget.searchIconSize,
+                  ),
+                ),
+              )
+            : widget.textFieldState == FloodingTextFieldState.password
+                ? GestureDetector(
+                    onTap: () => setState(() {
+                      _isObscureText = !_isObscureText;
+                    }),
+                    child: Padding(
+                      padding: widget.passwordIconPadding,
+                      child: _isObscureText
+                          ? FloodingIcon.eyeClose(
+                              color: widget.passwordOffIconColor,
+                              height: widget.passwordIconSize,
+                              width: widget.passwordIconSize,
+                            )
+                          : FloodingIcon.eyeOpen(
+                              color: widget.passwordOnIconColor,
+                              height: widget.passwordIconSize,
+                              width: widget.passwordIconSize,
+                            ),
+                    ),
+                  )
+                : null,
+        filled: true,
+        fillColor: widget.backgroundColor,
+        contentPadding: widget.contentPadding,
+        hintText: widget.hintText,
+        hintStyle: widget.hintStyle.copyWith(
+          color: widget.hintColor,
         ),
-      ],
+        errorStyle: widget.errorStyle.copyWith(
+          color: widget.errorColor,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: widget.borderRadius,
+          borderSide: BorderSide.none,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: widget.borderRadius,
+          borderSide: widget.errorBorderSide,
+        ),
+      ),
     );
   }
 }
